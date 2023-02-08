3ede04cd4efbe6d82abf2c69ba501470cba2b5df
HoneyB <qudgus9601@naver.com>
2023-02-08 23:54:07 +0900

    [FIX] 기타 설정 수정

--- server/blockchain_server/bin/index.js
+++ server/blockchain_server/bin/index.js
@@ -6,6 +6,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var dotenv_1 = __importDefault(require("dotenv"));
 var mongoose_1 = __importDefault(require("mongoose"));
+var cors_1 = __importDefault(require("cors"));
+var cookie_parser_1 = __importDefault(require("cookie-parser"));
+var morgan_1 = __importDefault(require("morgan"));
+var express_session_1 = __importDefault(require("express-session"));
+var fs_1 = __importDefault(require("fs"));
+var https_1 = __importDefault(require("https"));
+var index_1 = __importDefault(require("./routes/index"));
 var app = (0, express_1.default)();
 dotenv_1.default.config();
 mongoose_1.default
@@ -15,6 +22,27 @@ mongoose_1.default
     console.log("🍀 MongoDB Connected");
 })
     .catch(function (err) { });
-app.listen(process.env.SERVER_PORT, function () {
-    console.log("📚 Blockchain Server Open");
+app.use((0, morgan_1.default)("dev"));
+app.use(express_1.default.json());
+app.use((0, cookie_parser_1.default)());
+app.use((0, cors_1.default)({
+    origin: "https://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+}));
+app.use((0, express_session_1.default)({
+    resave: false,
+    saveUninitialized: false,
+    secret: "secret",
+}));
+app.use("/api/v1", index_1.default);
+https_1.default
+    .createServer({
+    key: fs_1.default.readFileSync(__dirname + "/key.pem", "utf-8"),
+    cert: fs_1.default.readFileSync(__dirname + "/cert.pem", "utf-8"),
+}, app.get("/", function (req, res) {
+    res.status(200).json({ message: "👋 This Is Blockchain Server" });
+}))
+    .listen(process.env.SERVER_PORT, function () {
+    console.log("📚 Blockchain Server Open In " + process.env.SERVER_PORT);
 });
